a13344825e97e2da0711f2e92c1e294499828641
unknown <hamzashahd001@gmail.com>
2023-10-15 17:41:24 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -9,3 +9,35 @@ icon.addEventListener("click", () => {
   bar.classList.toggle("active");
   clos.classList.toggle("activee");
 });
+
+//MODAL
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".closeM");
+let quotes = document.querySelectorAll(".feature .col");
+
+quotes.forEach(function (e) {
+  e.addEventListener("click", function () {
+    var quote = e.innerText;
+    console.log(quote);
+    var source = "Riyad alsalihen";
+    openModal(quote, source);
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+function openModal(quote, source) {
+  var qt = document.getElementById("qt");
+  var st = document.getElementById("st");
+  qt.innerText = quote;
+  st.innerText = source;
+  modal.style.display = "block";
+}
