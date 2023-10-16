f5c783b8bb9301840cb739d7feeeee72f2dcb826
unknown <hamzashahd001@gmail.com>
2023-10-17 01:04:48 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-let icon = document.querySelector(".icons");
-let uls = document.querySelector(".menu");
-let menuLinks = document.querySelectorAll(".menu a");
-// console.log(menuLinks);
-let bar = document.getElementsByClassName("bar")[0];
-let clos = document.getElementsByClassName("close")[0];
-
-icon.addEventListener("click", () => {
-  uls.classList.toggle("act");
-  bar.classList.toggle("active");
-  clos.classList.toggle("activee");
-});
-//
-menuLinks.forEach(function (l) {
-  l.addEventListener("click", function (e) {
-    uls.classList.toggle("act");
-    bar.classList.toggle("active");
-    clos.classList.toggle("activee");
-  });
-});
-
 //MODAL
 let modal = document.querySelector(".modal");
 let closeBtn = document.getElementsByClassName("closeM")[0];
---
+++ js/share.js
@@ -0,0 +1,12 @@
+fetch("../html/share.html")
+  .then((response) => response.text())
+  .then((data) => {
+    const pars = new DOMParser();
+    const doc = pars.parseFromString(data, "text/html");
+    const head = doc.querySelector(".hd");
+    const foot = doc.querySelector(".ft");
+    document.querySelector("header").appendChild(head);
+    document.querySelector("footer").appendChild(foot);
+    eval(doc.querySelector("script").textContent);
+  })
+  .catch((error) => console.error(error));
