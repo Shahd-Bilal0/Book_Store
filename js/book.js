3ad8189797f5faceca814a06bf3ac385bba6078d
unknown <hamzashahd001@gmail.com>
2023-11-02 10:36:21 +0200

    PLP for the books page

---
+++ js/book.js
@@ -0,0 +1,33 @@
+//scroller
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+window.addEventListener("scroll", () => {
+  let scrollT = document.documentElement.scrollTop;
+  el.style.width = `${(scrollT / height) * 100}%`;
+});
+
+let btn = document.querySelector("button");
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+btn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+//get Data
+let products = document.querySelector(".products");
+let card = document.querySelector(".products .card");
+
+for (let i = 0; i < 16; i++) {
+  let clonedCard = card.cloneNode(true);
+  products.appendChild(clonedCard);
+  console.log(card);
+}
