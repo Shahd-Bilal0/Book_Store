e50c920c7d9db6e64a30491dd63e4797d9a6fabc
unknown <hamzashahd001@gmail.com>
2023-11-10 01:28:08 +0200

    PDP

--- js/book.js
+++ js/book.js
@@ -23,369 +23,88 @@ btn.onclick = function () {
   });
 };
 let temp;
-let MainProducts = [
-  {
-    title: "Whats Wrong with a pet Dinosaur",
-    price: 17,
-    discountPrice: 25,
-    imageUrl: "../assets/children/1.jpg",
-    categories: "children",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Silly Jokes",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/children/2.jpg",
-    categories: "children",
-    type: "Fantasy",
-  },
-  {
-    title: "the LEAF THIEF",
-    price: 10,
-    discountPrice: 20,
-    imageUrl: "../assets/children/3.jpg",
-    categories: "children",
-    type: "Action",
-  },
-  {
-    title: "LITTLE HOUSE AND FRIENDS",
-    price: 16,
-    discountPrice: 20,
-    imageUrl: "../assets/children/4.jpg",
-    categories: "children",
-    type: "Crime",
-  },
-  {
-    title: "My First Book of Planets",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/children/5.jpg",
-    categories: "children",
-    type: "Romance",
-  },
-  {
-    title: "The 50 States",
-    price: 11,
-    discountPrice: 20,
-    imageUrl: "../assets/children/6.jpg",
-    categories: "children",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Why a Son Needs a Dad",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/children/7.jpg",
-    categories: "children",
-    type: "Fantasy",
-  },
-  {
-    title: "Little Planets",
-    price: 12,
-    discountPrice: 20,
-    imageUrl: "../assets/children/8.jpg",
-    categories: "children",
-    type: "Action",
-  },
-  {
-    title: "Awesome Facts",
-    price: 15,
-    discountPrice: 25,
-    imageUrl: "../assets/children/9.jpg",
-    categories: "children",
-    type: "Crime",
-  },
-  {
-    title: "How to Catch a Mermaid",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/children/10.jpg",
-    categories: "children",
-    type: "Romance",
-  },
-  {
-    title: "If Animals Kissed Good Night",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/children/11.jpg",
-    categories: "children",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Why a Son Needs a Mom",
-    price: 15,
-    discountPrice: 20,
-    imageUrl: "../assets/children/12.jpg",
-    categories: "children",
-    type: "Fantasy",
-  },
-  {
-    title: "The Seeking Tree",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/children/13.jpg",
-    categories: "children",
-    type: "Action",
-  },
-  {
-    title: "Everything children' Science Experiments",
-    price: 30,
-    discountPrice: 40,
-    imageUrl: "../assets/children/14.jpg",
-    categories: "children",
-    type: "Crime",
-  },
-  {
-    title: "The Not So Special Fish",
-    price: 8,
-    discountPrice: 10,
-    imageUrl: "../assets/children/15.jpg",
-    categories: "children",
-    type: "Romance",
-  },
-  {
-    title: "The Wonderful Things You Will Be Hardcover",
-    price: 16,
-    discountPrice: 20,
-    imageUrl: "../assets/children/16.jpg",
-    categories: "children",
-    type: "Sci-Fi",
-  },
-  {
-    title: "How to Catch the Easter Bunny",
-    price: 15,
-    discountPrice: 20,
-    imageUrl: "../assets/children/17.jpg",
-    categories: "children",
-    type: "Fantasy",
-  },
-  {
-    title: "My Magical Choices",
-    price: 11,
-    discountPrice: 20,
-    imageUrl: "../assets/children/18.jpg",
-    categories: "children",
-    type: "Action",
-  },
-  {
-    title: "The Scarecrow",
-    price: 10,
-    discountPrice: 20,
-    imageUrl: "../assets/children/19.jpg",
-    categories: "children",
-    type: "Crime",
-  },
-  {
-    title: "Leaf Man",
-    price: 19,
-    discountPrice: 25,
-    imageUrl: "../assets/children/20.jpg",
-    categories: "children",
-    type: "Romance",
-  },
-  {
-    title: "What's In The Forest?",
-    price: 8,
-    discountPrice: 25,
-    imageUrl: "../assets/children/21.jpg",
-    categories: "children",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Harry Potter Coloring Book",
-    price: 17,
-    discountPrice: 25,
-    imageUrl: "../assets/Adults/1.jpg",
-    categories: "Adults",
-    type: "Fantasy",
-  },
-  {
-    title: "Harry Potter Magical Places",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/2.jpg",
-    categories: "Adults",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Harry Potter Origami Volume",
-    price: 10,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/3.jpg",
-    categories: "Adults",
-    type: "Fantasy",
-  },
-  {
-    title: "Relaxing Flowers",
-    price: 16,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/4.jpg",
-    categories: "Adults",
-    type: "Romance",
-  },
-  {
-    title: "Catching Fire",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/5.jpg",
-    categories: "Adults",
-    type: "Action",
-  },
-  {
-    title: "The Ballad of Songbirds and Snakes",
-    price: 11,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/6.jpg",
-    categories: "Adults",
-    type: "Fantasy",
-  },
-  {
-    title: "Vintage Christmas Village",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/7.jpg",
-    categories: "Adults",
-    type: "Romance",
-  },
-  {
-    title: "Unbroken",
-    price: 12,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/8.jpg",
-    categories: "Adults",
-    type: "Action",
-  },
-  {
-    title: "Vintage Cars and Trucks",
-    price: 15,
-    discountPrice: 25,
-    imageUrl: "../assets/Adults/9.jpg",
-    categories: "Adults",
-    type: "Crime",
-  },
-  {
-    title: "The Silent Wife",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/10.jpg",
-    categories: "Adults",
-    type: "Romance",
-  },
-  {
-    title: "It Ends with Us",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/11.jpg",
-    categories: "Adults",
-    type: "Fantasy",
-  },
-  {
-    title: "Maybe Someday",
-    price: 15,
-    discountPrice: 20,
-    imageUrl: "../assets/Adults/12.jpg",
-    categories: "Adults",
-    type: "Sci-Fi",
-  },
-  {
-    title: "Some Kind of Comfort",
-    price: 17,
-    discountPrice: 25,
-    imageUrl: "../assets/older/1.jpg",
-    categories: "older",
-    type: "Fantasy",
-  },
-  {
-    title: "TimeOuts Titanic",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/older/2.jpg",
-    categories: "older",
-    type: "Sci-Fi",
-  },
-  {
-    title: "The Way I Used to Be Paperback",
-    price: 10,
-    discountPrice: 20,
-    imageUrl: "../assets/older/3.jpg",
-    categories: "older",
-    type: "Fantasy",
-  },
-  {
-    title: "The Prison Healer",
-    price: 16,
-    discountPrice: 20,
-    imageUrl: "../assets/older/4.jpg",
-    categories: "older",
-    type: "Romance",
-  },
-  {
-    title: "Once Upon a Dream",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/older/5.jpg",
-    categories: "older",
-    type: "Action",
-  },
-  {
-    title: "LORE",
-    price: 11,
-    discountPrice: 20,
-    imageUrl: "../assets/older/6.jpg",
-    categories: "older",
-    type: "Fantasy",
-  },
-  {
-    title: "How I Magically Messed Up My Life",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/older/7.jpg",
-    categories: "older",
-    type: "Romance",
-  },
-  {
-    title: "The Girl in the Castlek",
-    price: 12,
-    discountPrice: 20,
-    imageUrl: "../assets/older/8.jpg",
-    categories: "older",
-    type: "Action",
-  },
-  {
-    title: "Stephen king",
-    price: 15,
-    discountPrice: 25,
-    imageUrl: "../assets/older/9.jpg",
-    categories: "older",
-    type: "Crime",
-  },
-  {
-    title: "The Island Paperback",
-    price: 13,
-    discountPrice: 20,
-    imageUrl: "../assets/older/10.jpg",
-    categories: "older",
-    type: "Romance",
-  },
-  {
-    title: "Hatchet Paperback",
-    price: 9,
-    discountPrice: 20,
-    imageUrl: "../assets/older/11.jpg",
-    categories: "older",
-    type: "Fantasy",
-  },
-  {
-    title: "47 Days",
-    price: 15,
-    discountPrice: 20,
-    imageUrl: "../assets/older/12.jpg",
-    categories: "older",
-    type: "Sci-Fi",
-  },
-];
+let MainProducts;
+//CATEGORIES
+let categories = document.querySelectorAll(".categories .cat");
+let tit = document.querySelector(".col-2 .sort-menu .categ");
+let main_cat = document.querySelector(".col-2 .sort-menu .main-cat");
+//
+const checkboxes = document.querySelectorAll(".checkbox");
+const checkboxeslab = document.querySelectorAll("label .lab");
+let type_nums = document.querySelectorAll(".bcat .num");
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    MainProducts = data.Products;
+  })
+  .then(() => waitData());
 
+function waitData() {
+  categories.forEach((e, index) => {
+    categorProd(0, index, e.children[0].innerHTML);
+    e.addEventListener("click", () => {
+      checkboxes.forEach((e) => {
+        e.checked = false;
+      });
+      categories.forEach((l) => {
+        l.classList.remove("active");
+      });
+      tit.innerHTML = e.children[0].innerHTML;
+      e.classList.add("active");
+      categorProd(0, index, e.children[0].innerHTML);
+    });
+  });
+  main_cat.addEventListener("click", (e) => {
+    tit.innerHTML = "All";
+    categories.forEach((l) => {
+      l.classList.remove("active");
+    });
+    categorProd(1);
+  });
+  function categorProd(x, y, s) {
+    switch (x) {
+      case 0:
+        temp = MainProducts.filter(
+          (p) => p.categories.toLowerCase() === s.toLowerCase()
+        );
+
+        categories[y].children[1].innerHTML = `(${temp.length})`;
+        break;
+      case 1:
+        temp = [...MainProducts];
+        tit.innerHTML = `All  (${temp.length})`;
+        break;
+      default:
+    }
+    numsT();
+    displayProduct(temp);
+  }
+  categorProd(1);
+  checkboxes.forEach((e) => {
+    e.addEventListener("change", updateSelectedChoices);
+  });
+}
+//BOOKS BY TYPE
+function numsT() {
+  checkboxeslab.forEach((e, index) => {
+    let tt;
+    tt = temp.filter((p) => p.type == e.textContent.trim());
+    type_nums[index].innerHTML = ` (${tt.length})`;
+  });
+}
+function updateSelectedChoices() {
+  let selectedChoices = [];
+  checkboxes.forEach((e, index) => {
+    if (e.checked) {
+      selectedChoices.push(checkboxeslab[index].textContent.trim());
+    }
+  });
+  const filteredProducts = temp.filter((p) => {
+    return selectedChoices.includes(p.type);
+  });
+  if (selectedChoices.length == 0) displayProduct(temp);
+  else displayProduct(filteredProducts);
+}
 function displayProduct(products) {
   let cp = [...products].sort(compareByPriceAscending);
   const itemsPerPage = 9;
@@ -451,6 +170,7 @@ function displayProduct(products) {
 
     for (let i = start; i < end && i < products.length; i++) {
       addCard(
+        products[i].id,
         products[i].imageUrl,
         products[i].title,
         products[i].price,
@@ -458,27 +178,34 @@ function displayProduct(products) {
       );
     }
   }
-  function addCard(imageUrl, title, price, discountPrice) {
+  function addCard(id, imageUrl, title, price, discountPrice) {
     let newCard = document.createElement("div");
     newCard.classList.add("card");
     newCard.innerHTML = `
-          <img width="210px"  decoding="async" src="${imageUrl}" alt="" />
-          <div class="content">
-            <h6>${title}</h6>
-            <div class="pri">
-              <h6>$${price}</h6>
-              <h6><del>$${discountPrice}</del></h6>
+            <img width="210px"  decoding="async" src="${imageUrl}" alt="" />
+            <div class="content">
+            <input type="hidden" value="${id}">
+              <h6>${title}</h6>
+              <div class="pri">
+                <h6>$${price}</h6>
+                <h6><del>$${discountPrice}</del></h6>
+              </div>
             </div>
-          </div>
-          <div class="info">
-            <a href="" class="btn"><i class="icon fas fa-heart"></i></a>
-            <a href="" class="btn"><i class="icon fas fa-shopping-cart"></i></a>
-            <a href="" class="btn"><i class="icon fas fa-eye"></i></a>
-          </div>
-        `;
+            <div class="info">
+              <a href="" class="btn"><i class="icon fas fa-heart"></i></a>
+              <a href="" class="btn"><i class="icon fas fa-shopping-cart"></i></a>
+              <a href="" class="btn"><i class="icon fas fa-eye"></i></a>
+            </div>
+          `;
     productsD.appendChild(newCard);
   }
-
+  let cards = document.querySelectorAll(".products .card");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      var hiddenInputValue = card.querySelector('input[type="hidden"]').value;
+      window.location.href = `../html/product-page.html#pdp/?id=${hiddenInputValue}`;
+    });
+  });
   function updatePagination() {
     const totalPages = Math.ceil(products.length / itemsPerPage);
     let ulP = document.querySelector(".pagination .second-ul");
@@ -571,7 +298,6 @@ function displayProduct(products) {
     updatePagination();
   }
 }
-
 function compareByNameAscending(a, b) {
   const titleA = a.title.toUpperCase();
   const titleB = b.title.toUpperCase();
@@ -592,85 +318,4 @@ function compareByPriceAscending(a, b) {
 }
 function compareByPriceDescending(a, b) {
   return b.price - a.price;
-}
-//CATEGORIES
-let categories = document.querySelectorAll(".categories .cat");
-let tit = document.querySelector(".col-2 .sort-menu .categ");
-let main_cat = document.querySelector(".col-2 .sort-menu .main-cat");
-//
-const checkboxes = document.querySelectorAll(".checkbox");
-const checkboxeslab = document.querySelectorAll("label .lab");
-let type_nums = document.querySelectorAll(".bcat .num");
-categories.forEach((e, index) => {
-  categorProd(0, index, e.children[0].innerHTML);
-  e.addEventListener("click", () => {
-    checkboxes.forEach((e) => {
-      e.checked = false;
-    });
-    categories.forEach((l) => {
-      l.classList.remove("active");
-    });
-    tit.innerHTML = e.children[0].innerHTML;
-    e.classList.add("active");
-    categorProd(0, index, e.children[0].innerHTML);
-  });
-});
-main_cat.addEventListener("click", (e) => {
-  tit.innerHTML = "All";
-  categories.forEach((l) => {
-    l.classList.remove("active");
-  });
-  categorProd(1);
-});
-
-function categorProd(x, y, s) {
-  switch (x) {
-    case 0:
-      temp = MainProducts.filter(
-        (p) => p.categories.toLowerCase() === s.toLowerCase()
-      );
-
-      categories[y].children[1].innerHTML = `(${temp.length})`;
-      break;
-    case 1:
-      temp = [...MainProducts];
-      tit.innerHTML = `All  (${temp.length})`;
-      break;
-    default:
-  }
-  numsT();
-  displayProduct(temp);
-}
-categorProd(1);
-//BOOKS BY TYPE
-function numsT() {
-  console.log(temp);
-  checkboxeslab.forEach((e, index) => {
-    let tt;
-    tt = temp.filter((p) => p.type == e.textContent.trim());
-
-    type_nums[index].innerHTML = ` (${tt.length})`;
-  });
-}
-
-checkboxes.forEach((e) => {
-  e.addEventListener("change", updateSelectedChoices);
-});
-
-function updateSelectedChoices() {
-  let selectedChoices = [];
-  checkboxes.forEach((e, index) => {
-    if (e.checked) {
-      selectedChoices.push(checkboxeslab[index].textContent.trim());
-    }
-  });
-  console.log(selectedChoices);
-
-  const filteredProducts = temp.filter((p) => {
-    return selectedChoices.includes(p.type);
-  });
-  console.log(filteredProducts);
-
-  if (selectedChoices.length == 0) displayProduct(temp);
-  else displayProduct(filteredProducts);
 }
--- js/pdp.js
+++ js/pdp.js
@@ -22,33 +22,7 @@ btn.onclick = function () {
     behavior: "smooth",
   });
 };
-let selectedProduct = {
-  title: "the LEAF THIEF",
-  price: 150,
-  discountPrice: 20,
-  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-    assumenda aliquid quae eaque animi id? Ad adipisci porro enim nam
-    doloremque rerum beatae ratione neque. Nihil blanditiis pariatur
-    voluptates cupiditate? Lorem ipsum dolor sit amet consectetur
-    adipisicing elit. Ab quo officiis itaque quaerat ipsum, suscipit
-    vel a pariatur incidunt velit iste tempore tempora necessitatibus
-    maiores deleniti quos quasi. Debitis, cupiditate.`,
-  imageUrl: "../assets/children/3.jpg",
-  otherImages: [
-    "../assets/children/3b.jpg",
-    "../assets/children/3c.jpg",
-    "../assets/children/3d.jpg",
-    "../assets/children/3e.jpg",
-    "../assets/children/3f.jpg",
-    "../assets/children/3g.jpg",
-    "../assets/children/3h.jpg",
-    "../assets/children/3i.jpg",
-  ],
-  categories: "children",
-  type: "Action",
-};
-
-//PDP
+let selectedProduct;
 let title = document.querySelector(".productName");
 let price = document.querySelector(".productPrice");
 let desc = document.querySelector(".productDescription");
@@ -60,91 +34,102 @@ let Secondcarousel = document.querySelector(".second-carousel");
 let prevButton = document.getElementById("prevC");
 let nextButton = document.getElementById("nextC");
 let currentIndex = 0;
-//data
+const queryString = window.location.href;
+var givenId = queryString.split("=")[1];
+console.log(givenId);
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    MainProducts = data.Products;
+    selectedProduct = MainProducts[givenId - 1];
+  })
+  .then(() => waitData());
 
-title.innerHTML = `${selectedProduct.title.toUpperCase()}`;
-price.innerHTML = `${selectedProduct.price}$`;
-desc.innerHTML = `${selectedProduct.description}`;
-//carousel
-for (let i = 0; i < selectedProduct.otherImages.length; i++) {
-  let carouselItem = document.createElement("div");
-  carouselItem.classList.add("carousel-item");
-  let carouselItem2 = document.createElement("div");
-  carouselItem2.classList.add("carousel-item-two");
-  if (i == 0) {
-    carouselItem.classList.add("active");
-    carouselItem2.classList.add("active");
+function waitData() {
+  console.log(selectedProduct);
+  //data
+  title.innerHTML = `${selectedProduct.title.toUpperCase()}`;
+  price.innerHTML = `${selectedProduct.price}$`;
+  desc.innerHTML = `${selectedProduct.description}`;
+  //carousel
+  for (let i = 0; i < selectedProduct.otherImages.length; i++) {
+    let carouselItem = document.createElement("div");
+    carouselItem.classList.add("carousel-item");
+    let carouselItem2 = document.createElement("div");
+    carouselItem2.classList.add("carousel-item-two");
+    if (i == 0) {
+      carouselItem.classList.add("active");
+      carouselItem2.classList.add("active");
+    }
+    let img = document.createElement("img");
+    img.src = selectedProduct.otherImages[i];
+    img.alt = "Image";
+    let img2 = document.createElement("img");
+    img2.src = selectedProduct.otherImages[i];
+    img2.alt = "Image";
+
+    carouselItem.appendChild(img);
+    carouselItem2.appendChild(img2);
+    carousel.appendChild(carouselItem);
+    Secondcarousel.appendChild(carouselItem2);
   }
-  let img = document.createElement("img");
-  img.src = selectedProduct.otherImages[i];
-  img.alt = "Image";
-  let img2 = document.createElement("img");
-  img2.src = selectedProduct.otherImages[i];
-  img2.alt = "Image";
-
-  carouselItem.appendChild(img);
-  carouselItem2.appendChild(img2);
-  carousel.appendChild(carouselItem);
-  Secondcarousel.appendChild(carouselItem2);
-}
-let carouselItems = document.querySelectorAll(".carousel-item");
-let SecondCarouselItems = document.querySelectorAll(".carousel-item-two");
-console.log(SecondCarouselItems);
-SecondCarouselItems.forEach((e, index) => {
-  e.addEventListener("click", () => {
-    console.log(index);
-    currentIndex = index;
-    showSlide(index);
+  let carouselItems = document.querySelectorAll(".carousel-item");
+  let SecondCarouselItems = document.querySelectorAll(".carousel-item-two");
+  SecondCarouselItems.forEach((e, index) => {
+    e.addEventListener("click", () => {
+      currentIndex = index;
+      showSlide(index);
+    });
   });
-});
-function showSlide(index) {
-  carouselItems.forEach((e, i) => {
-    if (i === index) {
-      e.classList.add("active");
-      SecondCarouselItems[i].classList.add("active");
-    } else {
-      e.classList.remove("active");
-      SecondCarouselItems[i].classList.remove("active");
+
+  function showSlide(index) {
+    carouselItems.forEach((e, i) => {
+      if (i === index) {
+        e.classList.add("active");
+        SecondCarouselItems[i].classList.add("active");
+      } else {
+        e.classList.remove("active");
+        SecondCarouselItems[i].classList.remove("active");
+      }
+    });
+  }
+  function changeSlide(direction) {
+    currentIndex = currentIndex =
+      (currentIndex + direction + carouselItems.length) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+  prevButton.addEventListener("click", () => {
+    changeSlide(-1);
+  });
+  nextButton.addEventListener("click", () => {
+    changeSlide(1);
+  });
+  function autoAdvance() {
+    changeSlide(1);
+    setTimeout(autoAdvance, 7000);
+  }
+  // Start the auto-advancing carousel
+  setTimeout(autoAdvance, 7000);
+
+  //quantity
+  // Decrease the value
+  minusBtn.addEventListener("click", () => {
+    let value = parseInt(input.value);
+    if (value > 1) {
+      value--;
+      input.value = value;
+    }
+    if (value == 1) {
+      console.log("hi");
+      minusBtn.classList.add("disabled");
     }
   });
-}
-function changeSlide(direction) {
-  console.log("before" + currentIndex);
-  currentIndex = currentIndex =
-    (currentIndex + direction + carouselItems.length) % carouselItems.length;
-  console.log("after" + currentIndex);
-  showSlide(currentIndex);
-}
-prevButton.addEventListener("click", () => {
-  changeSlide(-1);
-});
-nextButton.addEventListener("click", () => {
-  changeSlide(1);
-});
-function autoAdvance() {
-  changeSlide(1);
-  setTimeout(autoAdvance, 7000);
-}
-// Start the auto-advancing carousel
-setTimeout(autoAdvance, 7000);
-
-//quantity
-// Decrease the value
-minusBtn.addEventListener("click", () => {
-  let value = parseInt(input.value);
-  if (value > 1) {
-    value--;
+  // Increase the value
+  plusBtn.addEventListener("click", () => {
+    let value = parseInt(input.value);
+    value++;
     input.value = value;
-  }
-  if (value == 1) {
-    console.log("hi");
-    minusBtn.classList.add("disabled");
-  }
-});
-// Increase the value
-plusBtn.addEventListener("click", () => {
-  let value = parseInt(input.value);
-  value++;
-  input.value = value;
-  minusBtn.classList.remove("disabled");
-});
+    minusBtn.classList.remove("disabled");
+  });
+}
+//PDP
