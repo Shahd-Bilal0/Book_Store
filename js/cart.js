7100022b5811e49075dbe7bc3309b6f84de82e61
Shahd-Bilal0 <hamzashahd001@gmail.com>
2023-11-23 10:27:31 +0200

    cart

---
+++ js/cart.js
@@ -0,0 +1,24 @@
+//scroller
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+window.addEventListener("scroll", () => {
+  let scrollT = document.documentElement.scrollTop;
+  el.style.width = `${(scrollT / height) * 100}%`;
+});
+
+let btn = document.querySelector("button");
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+btn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
